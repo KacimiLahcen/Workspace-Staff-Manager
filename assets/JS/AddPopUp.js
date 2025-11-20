722d02b28d23e5eec2e6846058c464370f2c9f01
KacimiLahcen <kacimi.lahcen88@gmail.com>
2025-11-20 01:14:05 +0100

    I worked on the adding workers form;using html,css,js

---
+++ assets/JS/AddPopUp.js
@@ -0,0 +1,26 @@
+function openPopup(){
+  let open = document.getElementById("overlay");
+  open.style.display = "flex";
+}
+
+function closePopup(){
+  let close = document.getElementById("overlay");
+  close.style.display = "none";
+}
+
+
+
+let expCount = 1;
+
+function addExperience(){
+  const box = document.createElement("div");
+  box.className = "exp-box";
+  box.innerHTML = `
+    <span class="remove-exp" onclick="this.parentNode.remove()">×</span>
+    <strong>Expérience ${expCount++}</strong><br>
+    <input placeholder="Entreprise">
+    <input placeholder="Poste">
+    <input placeholder="Durée">
+  `;
+  document.getElementById("expContainer").appendChild(box);
+}
